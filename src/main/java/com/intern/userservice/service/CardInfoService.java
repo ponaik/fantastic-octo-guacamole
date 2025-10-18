1c9de1bed22665cad0321781e19ebf9361a2a104
Ioann Yaromenka <pon@Ioanns-MacBook-Air.local>
2025-10-18 18:57:18 +0300

    CardInfo service methods for create, delete, findALl, findById

--- src/main/java/com/intern/userservice/service/CardInfoService.java
+++ src/main/java/com/intern/userservice/service/CardInfoService.java
@@ -1,4 +1,56 @@
 package com.intern.userservice.service;
 
+import com.intern.userservice.dto.CardInfoCreateDto;
+import com.intern.userservice.dto.CardInfoResponse;
+import com.intern.userservice.mapper.CardInfoMapper;
+import com.intern.userservice.model.CardInfo;
+import com.intern.userservice.model.User;
+import com.intern.userservice.repository.CardInfoRepository;
+import com.intern.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CardInfoService {
+
+    private final CardInfoRepository cardInfoRepository;
+    private final UserRepository userRepository;
+    private final CardInfoMapper cardInfoMapper;
+
+
+    public Optional<CardInfoResponse> getCardById(Long id) {
+        return cardInfoRepository.findCardByIdJPQL(id)
+                .map(cardInfoMapper::toCardInfoResponse);
+    }
+
+    public Page<CardInfoResponse> getAllCards(Pageable pageable) {
+        return cardInfoRepository.findAll(pageable)
+                .map(cardInfoMapper::toCardInfoResponse);
+    }
+
+    @Transactional
+    public void deleteCardById(Long id) {
+        if (!cardInfoRepository.existsById(id)) {
+            throw new IllegalArgumentException("Card with id " + id + " not found");
+        }
+        cardInfoRepository.deleteById(id);
+    }
+
+    public CardInfoResponse createCard(CardInfoCreateDto dto) {
+        User user = userRepository.findById(dto.userId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        CardInfo card = cardInfoMapper.fromCardInfoCreateDto(dto);
+        card.setUser(user);
+
+        CardInfo saved = cardInfoRepository.save(card);
+        return cardInfoMapper.toCardInfoResponse(saved);
+    }
 }
+
